6438a126c33073d3ed39373481c0255ffb365f62
rogirocz2 <rogirocz2@gmail.com>
2025-08-31 19:42:19 +0000

    update file to new configurations

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-require('dotenv').config()
-
+import loadConfig from './config/readConfig.js';
 import express from 'express';
+import Logger from './logs/Logger.js';
+
+loadConfig();
+
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json(req.query);
+});
 
 const port = parseInt(process.env.PORT) || 3000;
 app.listen(port, () => {
