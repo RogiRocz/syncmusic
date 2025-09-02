550df4def5a296f253dd322aa06275f2d19181f0
rogirocz2 <rogirocz2@gmail.com>
2025-09-02 13:58:28 +0000

    refact middleware of logging

--- middleware/routesLogs.js
+++ middleware/routesLogs.js
@@ -1,7 +1,16 @@
-import logger from '../logs/Logger'
+import logger from '../logs/Logger.js'
 
-export function routeLogger(req, res, next) {
-    const logMessage = `[${req.method}] - Status: ${res.statusCode} - Path: ${req.originalUrl} - IP: ${req.ip}`
-    logger.info(logMessage)
-    next()
+const routesLogs = (req, res, next) => {
+    res.on('finish', () => {
+        const finishMessage = `[${req.method}] ${req.originalUrl} - STATUS: ${res.statusCode} - IP: ${req.ip}`;
+        if (res.statusCode >= 400) {
+            logger.warn(finishMessage);
+        } else {
+            logger.info(finishMessage);
+        }
+    });
+
+    next();
 }
+
+export default routesLogs;
