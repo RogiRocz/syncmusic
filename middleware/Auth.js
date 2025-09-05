2d5419f33139acd75e386f77924cde5fcd5c9c0a
rogirocz2 <rogirocz2@gmail.com>
2025-09-05 19:16:24 +0000

    add hasRole to check level of authorization to access some paths

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,14 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
-import firebaseAdmin from '../config/firebase.js';
-import logger from '../logs/Logger.js';
 import validateToken from '../utils/FirebaseAuth.js';
+import Logger from '../logs/Logger.js';
 
-export async function Auth(req, res, next) {
+const isAuthenticaded = async (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
 
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            logger.warn('Acesso negado: Bearer token não fornecido ou mal formatado');
+            Logger.warn('Acesso negado: Bearer token não fornecido ou mal formatado');
             return res.status(StatusCodes.UNAUTHORIZED).json({
                 error: 'Acesso negado: Bearer token não fornecido ou mal formatado.',
                 code: 'MISSING_AUTH_TOKEN'
@@ -16,16 +15,35 @@ export async function Auth(req, res, next) {
         }
 
         const idToken = authHeader.split(' ')[1];
-
         const decodedToken = await validateToken(idToken);
-        
-        req.user = decodedToken;
+
+        req.user = {
+            uid: decodedToken.uid,
+            role: decodedToken.role || 'user'
+        };
         next();
     } catch (error) {
-        logger.error(`Erro de autenticação final no middleware: ${error.message}`);
-        return res.status(StatusCodes.UNAUTHORIZED).json({ 
+        Logger.error(`Erro de autenticação final no middleware: ${error.message}`);
+        return res.status(StatusCodes.UNAUTHORIZED).json({
             message: 'Acesso não autorizado.',
             code: error.code || 'AUTH_ERROR'
         });
     }
+};
+
+const hasRole = async (role) => {
+    return (req, res, next) => {
+        if (!req.user || req.user.role != role) {
+            Logger.warn(`Forbidden: User ${req.user?.uid} with role ${req.user?.role} tried to access a route requiring ${role}`);
+            return res.status(StatusCodes.FORBIDDEN).json({
+                message: `Access denied: You need to have ${role} role to access this route.}`
+            });
+        }
+        next();
+    }
+};
+
+export {
+    isAuthenticaded,
+    hasRole
 }
