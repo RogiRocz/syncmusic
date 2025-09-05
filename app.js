91720e55ed2c5ec89f76f990602fe150a3d84af7
rogirocz2 <rogirocz2@gmail.com>
2025-09-05 19:17:41 +0000

    add new routes to application

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import './config/readConfig.js';
 import express from 'express';
 import routesLogs from './middleware/routesLogs.js';
 import userRoutes from './routes/User.js';
+import playlistRoutes from './routes/Playlist.js';
+import trackRoutes from './routes/Track.js';
 import configRoutes from './routes/Config.js';
+import { isAuthenticaded } from './middleware/Auth.js';
 
 const app = express();
 
@@ -13,8 +16,10 @@ app.use(routesLogs);
 app.use(express.static('public')); 
 
 // Rotas da API
-app.use('/api/users', userRoutes);
-app.use('/api/config', configRoutes);
+app.use('/api/v1/users', isAuthenticaded, userRoutes);
+app.use('/api/v1/playlists', isAuthenticaded, playlistRoutes);
+app.use('/api/v1/tracks', isAuthenticaded, trackRoutes);
+app.use('/api/v1/config', configRoutes);
 
 // Rota de Teste
 app.get('/', (req, res) => {
